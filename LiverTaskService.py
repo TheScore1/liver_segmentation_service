15f63033aa2ae18e841ea453d4742121cb6aa9a2
TheScore1 <113776816+TheScore1@users.noreply.github.com>
2024-05-31 05:31:59 +0500

    Update LiverTaskService.py to 3

--- LiverTaskService.py
+++ LiverTaskService.py
@@ -7,7 +7,9 @@ from torchvision import transforms
 from monai.networks.nets import UNet
 import nibabel as nib
 import time
-import io
+import tempfile
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 # Загрузка сохраненной модели
 def load_model(model_path, device):
@@ -25,13 +27,18 @@ def load_model(model_path, device):
 
 # Функция для предсказания маски
 def predict_mask(image, model, device):
-    transform = transforms.Compose([
-        transforms.Grayscale(num_output_channels=1),
-        transforms.ToTensor(),
-        transforms.Resize((512, 512))
+    if image.mode == 'RGB':
+        image = image.convert('L')
+
+    transform = A.Compose([
+        A.Resize(512, 512),
+        A.Normalize(mean=(0.5,), std=(0.5,)),
+        ToTensorV2()
     ])
     
-    image = transform(image).unsqueeze(0).to(device)
+    transformed = transform(image=np.array(image))
+    image = transformed['image'].unsqueeze(0).to(device)
+
     with torch.no_grad():
         output = model(image)
         pred = torch.sigmoid(output).cpu().numpy().squeeze()
@@ -46,30 +53,55 @@ def show_image_and_mask(image, mask):
     axes[1].set_title("Predicted Mask")
     st.pyplot(fig)
 
+# Функция для загрузки и обработки NIfTI файлов
+def load_nifti_file(file):
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.nii') as tmp:
+            tmp.write(file.read())
+            tmp_path = tmp.name
+        nifti_img = nib.load(tmp_path)
+        nifti_data = nifti_img.get_fdata()
+        return nifti_data
+    except Exception as e:
+        st.error(f"Error loading NIfTI file: {e}")
+        return None
+
+def normalize_nifti_image(nifti_slice):
+    nifti_slice = (nifti_slice - nifti_slice.min()) / (nifti_slice.max() - nifti_slice.min())
+    nifti_slice = (nifti_slice * 255).astype(np.uint8)
+    return nifti_slice
+
+
 # Настройки Streamlit
 st.title("Liver Segmentation using UNet")
 st.write("Upload an image to get the liver segmentation mask.")
 
 # Загрузка модели
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_path = "liver_segmentation_model.pth"
+model_path = "e-18-iou-1.4925-liver_segmentation_model.pth"
 model = load_model(model_path, device)
 
 # Загрузка изображения
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "nii"])
+uploaded_file = st.file_uploader("Choose an image (jpg or nii)...", type=["jpg", "png", "nii"])
 
 if uploaded_file is not None:
-    if uploaded_file.name.endswith('.nii'):
-        # Чтение содержимого файла в байтовый поток
-        file_bytes = uploaded_file.read()
-        nii_image = nib.Nifti1Image.from_bytes(file_bytes)
-        nii_data = nii_image.get_fdata()
-        slice_index = st.slider("Select slice", 0, nii_data.shape[2] - 1, nii_data.shape[2] // 2)
-        image = nii_data[:, :, slice_index]
-        image = Image.fromarray((image / np.max(image) * 255).astype(np.uint8))
+    if uploaded_file.name.endswith(".nii"):
+        # Обработка NIfTI файла
+        nifti_data = load_nifti_file(uploaded_file)
+        if nifti_data is not None:
+            num_slices = nifti_data.shape[2]
+            slice_idx = st.slider("Select Slice", 0, num_slices - 1, num_slices // 2)
+            selected_slice = nifti_data[:, :, slice_idx]
+            normalized_slice = normalize_nifti_image(selected_slice)
+            image = Image.fromarray(normalized_slice, 'L')
+            ##image = Image.fromarray(np.uint8((selected_slice - selected_slice.min()) * 255 / (selected_slice.max() - selected_slice.min())), 'L')
+        else:
+            st.error("Failed to load NIfTI file.")
     else:
+        # Обработка jpg файла
         image = Image.open(uploaded_file)
-    
+        selected_slice = np.array(image)
+
     image_placeholder = st.empty()
     text_placeholder = st.empty()
     
@@ -87,6 +119,4 @@ if uploaded_file is not None:
     text_placeholder.empty()
 
     # Отображение результата
-    show_image_and_mask(np.array(image), mask)
-
-#streamlit run app.py
+    show_image_and_mask(selected_slice, mask)
